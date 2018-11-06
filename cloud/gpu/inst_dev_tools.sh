156d505e0c9bc5efc52e58dc27831f1490f5ec00
Alfred Thompson <alfred@ridecell.com>
2018-11-06 11:23:22 -0800

    Add more scripts from gpu node setup

---
+++ cloud/gpu/inst_dev_tools.sh
@@ -0,0 +1,15 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt-get install -y \
+     screen \
+     build-essential \
+     emacs24-lucid \
+     libvtk6-dev \
+     vtk6 \
+     doxygen \
+     python-pip \
+     python3-pip
+
+pip  install --upgrade pip
+pip3 install --upgrade pip
