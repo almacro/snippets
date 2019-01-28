e750c47962d46271183d31385f67af8992e5b291
Al Thompson <almacro@acm.org>
2019-01-27 21:15:36 -0800

    Add PyQuick problems and solutions

---
+++ python/learning/google/python-exercises/copyspecial/copyspecial.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+import sys
+import re
+import os
+import shutil
+import commands
+
+"""Copy Special exercise
+"""
+
+def copy_to_dir(todir, dirs):
+  if not os.path.exists(todir):
+    os.makedirs(todir)
+  for dir in dirs:
+    files = os.listdir(dir)
+    for file in files:
+      match = re.search(r'__\w+__', file)
+      if match:
+        path = os.path.abspath(os.path.join(dir, file))
+        shutil.copy(path, todir)
+
+def copy_to_zip(tozip, dirs):
+  files_to_zip = []
+  for dir in dirs:
+    files = os.listdir(dir)
+    for file in files:
+      match = re.search(r'__\w+__', file)
+      if match:
+        path = os.path.abspath(os.path.join(dir, file))
+        files_to_zip.append(path)
+  cmd = ' '.join(['zip','-j',tozip,' '.join(files_to_zip)])
+  print 'cmd to run:', cmd
+  status, output = commands.getstatusoutput(cmd)
+  if status:
+    sys.stderr.write('cmd error:' + output +'\n')
+    sys.exit(1)
+  print output
+  
+def echo_special(dirs):
+  for dir in dirs:
+    files = os.listdir(dir)
+    for file in files:
+      match = re.search(r'__\w+__', file)
+      if match:
+        print os.path.abspath(os.path.join(dir, file))
+
+def main():
+  # This basic command line argument parsing code is provided.
+  # Add code to call your functions below.
+
+  # Make a list of command line arguments, omitting the [0] element
+  # which is the script itself.
+  args = sys.argv[1:]
+  if not args:
+    print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
+    sys.exit(1)
+
+  # todir and tozip are either set from command line
+  # or left as the empty string.
+  # The args array is left just containing the dirs.
+  todir = ''
+  if args[0] == '--todir':
+    todir = args[1]
+    del args[0:2]
+
+  tozip = ''
+  if args[0] == '--tozip':
+    tozip = args[1]
+    del args[0:2]
+
+  if len(args) == 0:
+    print "error: must specify one or more dirs"
+    sys.exit(1)
+
+  for arg in args:
+    if not os.path.exists(arg):
+      sys.stderr.write('dir not found: ' + arg + '\n')
+      sys.exit(1)
+  
+  if todir:
+    copy_to_dir(todir, args)
+  elif tozip:
+    copy_to_zip(tozip, args)
+  else:
+    echo_special(args)
+
+if __name__ == "__main__":
+  main()
---
+++ python/learning/google/python3-exercises/basic/list2.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python -tt
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+# Additional basic list exercises
+
+# D. Given a list of numbers, return a list where
+# all adjacent == elements have been reduced to a single element,
+# so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
+# modify the passed in list.
+def remove_adjacent(nums):
+  dup = []
+  for n in range(len(nums)-1):
+    if nums[n] == nums[n+1]:
+      dup.append(n)
+  for d in sorted(dup, reverse=True):
+    del nums[d]
+  return nums
+
+# E. Given two lists sorted in increasing order, create and return a merged
+# list of all the elements in sorted order. You may modify the passed in lists.
+# Ideally, the solution should work in "linear" time, making a single
+# pass of both lists.
+def linear_merge(list1, list2):
+  c = []
+  while True:
+    a = list1 and list1.pop(-1)
+    b = list2 and list2.pop(-1)
+    if (a,b) == ([],[]):
+      break
+    if a and b:
+      if a > b:
+        c.append(a)
+        list2.append(b)
+      elif b > a:
+        c.append(b)
+        list1.append(a)
+      else:
+        c += [a,b]
+    elif a:
+      c.append(a)
+    else:
+      c.append(b)
+  c.reverse()
+  return c
+  
+# Note: the solution above is kind of cute, but unforunately list.pop(0)
+# is not constant time with the standard python list implementation, so
+# the above is not strictly linear time.
+# An alternate approach uses pop(-1) to remove the endmost elements
+# from each list, building a solution list which is backwards.
+# Then use reversed() to put the result back in the correct order. That
+# solution works in linear time, but is more ugly.
+
+
+# Simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+
+# Calls the above functions with interesting inputs.
+def main():
+  print('remove_adjacent')
+  test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
+  test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
+  test(remove_adjacent([]), [])
+
+  print('\nlinear_merge')
+  test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
+       ['aa', 'bb', 'cc', 'xx', 'zz'])
+  test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
+       ['aa', 'bb', 'cc', 'xx', 'zz'])
+  test(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
+       ['aa', 'aa', 'aa', 'bb', 'bb'])
+
+
+if __name__ == '__main__':
+  main()
