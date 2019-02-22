088a17f52555f198c6d6eb33b4392c537220fdf9
Al Thompson <almacro@acm.org>
2019-02-22 06:02:19 -0800

    Add wallet support

--- python/learning/block_crypto/node.py
+++ python/learning/block_crypto/node.py
@@ -1,13 +1,14 @@
 from uuid import uuid4
 
 from blockchain import Blockchain
-from verification import Verification as verifier
+from wallet import Wallet
+
+from utility.verification import Verification as verifier
 
 class Node():
     def __init__(self):
-        #self.id = str(uuid4())
-        self.id = 'Kwaku'
-        self.blockchain = Blockchain(self.id)
+        self.wallet = Wallet()
+        self.blockchain = Blockchain(self.wallet.public_key)
         
     @staticmethod
     def get_transaction_value():
@@ -27,7 +28,7 @@ class Node():
     def print_blockchain_elements(self):
         """Output the blockchain list to the console"""
         for block in self.blockchain.get_chain():
-            print('Outputting block:')
+            print('Found block:')
             print(block)
         else:
             print('-' * 20)
@@ -41,18 +42,26 @@ class Node():
             print('2: Mine a new block')
             print('3: Output the blockchain blocks')
             print('4: Check transaction integrity')
+            print('5: Create wallet')
+            print('6: Load wallet')
+            print('7: Save keys')
             print('q: Quit')
             user_choice = self.get_user_choice()
             if user_choice == '1':
                 tx_data = self.get_transaction_value()
+                sender = self.wallet.public_key
                 recipient, amount = tx_data
-                if self.blockchain.add_transaction(recipient, self.id, amount=amount):
+                signature = self.wallet.sign_transaction(sender, recipient, amount)
+                if self.blockchain.add_transaction(recipient, sender, signature, amount=amount):
                     print('Transaction added!')
                 else:
                     print('Transaction failed!')
                 print(self.blockchain.get_open_transactions())
             elif user_choice == '2':
-                self.blockchain.mine_block()
+                if self.blockchain.mine_block():
+                    print('New block added!')
+                else:
+                    print('Mining failed!')
             elif user_choice == '3':
                 self.print_blockchain_elements()
             elif user_choice == '4':
@@ -60,6 +69,14 @@ class Node():
                     print('All transactions are valid')
                 else:
                     print('There are invalid transactions')
+            elif user_choice == '5':
+                self.wallet.create_keys()
+                self.blockchain = Blockchain(self.wallet.public_key)
+            elif user_choice == '6':
+                self.wallet.load_keys()
+                self.blockchain = Blockchain(self.wallet.public_key)
+            elif user_choice == '7':
+                self.wallet.save_keys()
             elif user_choice == 'q':
                 waiting_for_input = False
             else:
@@ -68,7 +85,8 @@ class Node():
                 self.print_blockchain_elements()
                 print('Invalid blockchain!')
                 break
-            print('Balance of {}: {:6.2f}'.format(self.id, self.blockchain.get_balance(self.id)))
+            id = self.wallet.public_key
+            print('Balance of {}: {:6.2f}'.format(id, self.blockchain.get_balance(id)))
         print('Done!')
         
 if __name__ == '__main__':
