720252cb5e5b1324a6f2efcdbbc3a4f5a858c315
Al Thompson <almacro@acm.org>
2019-12-16 05:11:22 -0800

    Share scripts from LFS hacking.

---
+++ os/linux/lfs/pass-1/conf_gcc.sh
@@ -0,0 +1,21 @@
+../configure                                     \
+  --target=$LFS_TGT                              \
+  --prefix=/tools                                \
+  --with-glibc-version=2.11                      \
+  --with-sysroot=$LFS                            \
+  --with-newlib                                  \
+  --without-headers                              \
+  --with-local-prefix=/tools                     \
+  --with-native-system-header-dir=/tools/include \
+  --disable-nls                                  \
+  --disable-shared                               \
+  --disable-multilib                             \
+  --disable-decimal-float                        \
+  --disable-threads                              \
+  --disable-libatomic                            \
+  --disable-libgomp                              \
+  --disable-libquadmath                          \
+  --disable-libssp                               \
+  --disable-libvtv                               \
+  --disable-libstdcxx                            \
+  --enable-languages=c,c++
---
+++ os/linux/lfs/pass-1/conf_glibc.sh
@@ -0,0 +1,6 @@
+../configure                         \
+  --prefix=/tools                    \
+  --host=$LFS_TGT                    \
+  --build=$(../scripts/config.guess) \
+  --enable-kernel=3.2                \
+  --with-headers=/tools/include
