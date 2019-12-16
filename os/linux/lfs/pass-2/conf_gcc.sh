208d37d4619867c0aff8d00758b726af5bbf9de2
Al Thompson <27452+almacro@users.noreply.github.com>
2019-12-16 05:11:22 -0800

    Share scripts from LFS hacking.

---
+++ os/linux/lfs/pass-2/conf_gcc.sh
@@ -0,0 +1,13 @@
+CC=$LFS_TGT-gcc                                  \
+CXX=$LFS_TGT-g++                                 \
+AR=$LFS_TGT-ar                                   \
+RANLIB=$LFS_TGT-ranlib                           \
+../configure                                     \
+  --prefix=/tools                                \
+  --with-local-prefix=/tools                     \
+  --with-native-system-header-dir=/tools/include \
+  --enable-languages=c,c++                       \
+  --disable-libstdcxx-pch                        \
+  --disable-multilib                             \
+  --disable-bootstrap                            \
+  --disable-libgomp
---
+++ os/linux/lfs/pass-2/fix_gcc_unpack_tarballs.sh
@@ -0,0 +1,6 @@
+tar -xf ../mpfr-4.0.2.tar.xz
+mv -v mpfr-4.0.2 mpfr
+tar -xf ../gmp-6.1.2.tar.xz
+mv -v gmp-6.1.2 gmp
+tar -xf ../mpc-1.1.0.tar.gz
+mv -v mpc-1.1.0 mpc
