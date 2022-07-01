3d61c6d7d1405d6252486311d5e8e2db42282d34
Alfred Thompson <alfred.thompson@getcruise.com>
2022-06-30 18:09:11 -0700

    Edit example to add comments

--- tcl/simple/fact.tcl
+++ tcl/simple/fact.tcl
@@ -2,8 +2,19 @@
 #
 # compute factorial
 
+# recursive
 proc fact n {expr {$n<2? 1: $n * [fact [incr n -1]]}}
 
+# iterative
+# proc fact n {
+#   set count $n
+#   for {set total 1} {$count > 0} {incr count -1} {
+#     set total [expr $total * $count]
+#   }
+#   return $total
+# }
+
+# testing
 #proc fact n {
 #  set n
 #}
